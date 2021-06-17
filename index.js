c59eb68776b8721eff672096a913c471ee29978a
Rettey-G <70497050+Rettey-G@users.noreply.github.com>
2021-06-17 13:04:22 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+}
+  
+
+document.querySelector('#contact-form').addEventListener('submit', function (e) {
+    e.preventDefault();
+    e.target.elements.name.value = '';
+    e.target.elements.email.value = '';
+    e.target.elements.message.value = '';
+  });
